4fe8c8f9b6b8b2cd4cb8a703de9fdfe52edd6103
Cristian Alveiro Mejia Obando <alveiro09@hotmail.com>
2020-09-18 10:46:10 -0500

    add  Advertise the API versions supported for the particular endpoint

--- Startup.cs
+++ Startup.cs
@@ -64,10 +64,12 @@ namespace Demos.API
             // Add API Versioning to the service container to your project
             services.AddApiVersioning(config =>
             {
-                // Specify the default API Version as 1.0
+                // Specify the default API Version
                 config.DefaultApiVersion = new ApiVersion(1, 0);
                 // If the client hasn't specified the API version in the request, use the default API version number 
                 config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
             });
         }
 
